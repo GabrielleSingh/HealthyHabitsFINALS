965cfa774cba1471aa3ba0ca130c3a8f69755242
GabrielleSingh <59948459+GabrielleSingh@users.noreply.github.com>
2021-12-01 07:03:39 +0800

    Add files via upload

---
+++ Controllers/HabitController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using HealthyHabits.Data;
+using HealthyHabits.Models;
+
+namespace HealthyHabits.Controllers
+{
+    public class HabitController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+
+        public HabitController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var list = _context.Habits.ToList();
+            return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Habit record)
+        {
+            var habit = new Habit();
+            {
+                habit.HabitName = record.HabitName;
+                habit.HabitDesc = record.HabitDesc;
+                habit.IncompHB = record.IncompHB;
+                habit.BadgeID = record.BadgeID;
+                habit.ID = record.ID;
+                habit.Type = record.Type;
+            }
+
+            _context.Habits.Add(habit);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+
+
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var habit = _context.Habits.Where(i => i.HabitID == id).SingleOrDefault();
+            if( habit == null)
+            {
+                return RedirectToAction("Index");
+
+            }
+
+            return View(habit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int? id, Habit record)
+        {
+            var habit = _context.Habits.Where(i => i.HabitID == id).SingleOrDefault();
+            habit.HabitName = record.HabitName;
+            habit.HabitDesc = record.HabitDesc;
+            habit.IncompHB = record.IncompHB;
+            habit.BadgeID = record.BadgeID;
+            habit.ID = record.ID;
+            habit.Type = record.Type;
+
+
+            _context.Habits.Update(habit);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+             if(id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var habit = _context.Habits.Where(i => i.HabitID == id).SingleOrDefault();
+            if(habit == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+
+            _context.Habits.Remove(habit);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
